1daa7b8957c165bdc597d83fbd5bdf5fa6ad6bc0
aymenzouaoui <118661290+aymenzouaoui@users.noreply.github.com>
2024-04-25 20:14:39 +0100

    project

---
+++ crosschat/ChatsView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatsView.swift
+//  crosschat
+//
+//  Created by arafetksiksi on 25/4/2024.
+//
+
+import SwiftUI
+
+struct ChatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatsView()
+}
---
+++ crosschat/MessengerView.swift
@@ -0,0 +1,18 @@
+//
+//  MessengerView.swift
+//  crosschat
+//
+//  Created by arafetksiksi on 25/4/2024.
+//
+
+import SwiftUI
+
+struct MessengerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessengerView()
+}
