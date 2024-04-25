52515da10ad1b0a71520728676a86bca45e3daa2
aymenzouaoui <118661290+aymenzouaoui@users.noreply.github.com>
2024-04-25 22:25:12 +0100

    ui emoji

--- crosschat/MessagesDataModel.swift
+++ crosschat/MessagesDataModel.swift
@@ -15,14 +15,15 @@ struct MessagesStructure: Identifiable {
     var sender: String
     var content: String
     var timestamp: String
+    var emoji: String? // Added emoji property
 }
 
 // Data storage
 let MessageData = [
 
-    MessagesStructure(unreadIndicator: "", avatar: "martin", sender: "You", content: "I don't know why people are so anti pineapple pizza. I kind of like it.", timestamp: "12:40 AM"),
-    MessagesStructure(unreadIndicator: "", avatar: "jeroen", sender: "Zach Friedman", content: "(Sad fact: you cannot search for a gif of the word “gif”, just gives you gifs.)", timestamp: "11:00 AM"),
-    MessagesStructure(unreadIndicator: "", avatar: "carla", sender: "You", content: "There's no way you'll be able to jump your motorcycle over that bus.", timestamp: "10:36 AM"),
-    MessagesStructure(unreadIndicator: "", avatar: "zain", sender: "Dee McRobie", content: "Tabs make way more sense than spaces. Convince me I'm wrong. LOL.", timestamp: "9:59 AM"),
+    MessagesStructure(unreadIndicator: "", avatar: "martin", sender: "You", content: "I don't know why people are so anti pineapple pizza. I kind of like it.", timestamp: "12:40 AM", emoji: nil),
+    MessagesStructure(unreadIndicator: "", avatar: "jeroen", sender: "Zach Friedman", content: "(Sad fact: you cannot search for a gif of the word “gif”, just gives you gifs.)", timestamp: "11:00 AM", emoji: nil),
+    MessagesStructure(unreadIndicator: "", avatar: "carla", sender: "You", content: "There's no way you'll be able to jump your motorcycle over that bus.", timestamp: "10:36 AM", emoji: nil),
+    MessagesStructure(unreadIndicator: "", avatar: "zain", sender: "Dee McRobie", content: "Tabs make way more sense than spaces. Convince me I'm wrong. LOL.", timestamp: "9:59 AM", emoji: nil),
   
 ]
